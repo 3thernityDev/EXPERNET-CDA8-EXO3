c6fc83b01c6efccabced118768e572abde0d99ae
3thernitydev <3thernitydev@gmail.com>
2024-10-24 09:30:53 +0400

    add some comment

--- website/index.php
+++ website/index.php
@@ -1,6 +1,8 @@
 <?php
+// Check if php is working
 echo ("Hello World" . "<br/>");
 
+// Test tableau
 $tableau = array(1, 2, 3, 4);
 
 echo $tableau[3];
@@ -8,3 +10,5 @@ echo $tableau[3];
 $tableau[3] = 6;
 
 echo $tableau[3];
+
+// if & else
