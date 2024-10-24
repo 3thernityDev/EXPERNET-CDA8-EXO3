259bd6deb2b1e03df48573a74efc7c68f80831ca
3thernitydev <3thernitydev@gmail.com>
2024-10-24 10:06:07 +0400

    add title and styles

---
+++ website/exo-3/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>EXPERNET | CDA8 EXO3</title>
+</head>
+
+<body>
+    <h1><?php echo ("La CDA8 vous souhaite une exelente journée ! ") ?></h1>
+</body>
+
+</html>
